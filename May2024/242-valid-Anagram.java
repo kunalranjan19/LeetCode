87c6d4c374354b9ece38c458033c4fe76ee529c1
Kunal Ranjan <ranjankunal103@gmail.com>
2024-05-22 23:24:14 +0530

    valid anagram

---
+++ May2024/242-valid-Anagram.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] c=s.toCharArray();
+        char[] d=t.toCharArray();
+        Arrays.sort(c);
+        Arrays.sort(d);
+        return Arrays.equals(c,d);
+    }
+}
